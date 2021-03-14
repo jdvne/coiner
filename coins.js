6682ee038c0d46722ed075cc1db8a77e1c199fdf
Joshua Devine <joshuaddevine@gmail.com>
2021-03-14 15:20:34 -0400

    updates during meeting 1

---
+++ coins.js
@@ -0,0 +1,80 @@
+chartColors = {
+	red: 'rgb(255, 99, 132)',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(201, 203, 207)'
+};
+
+
+function makeChart(name, initial){
+    var config = {
+        type: 'line',
+        data: {
+            labels: [""],
+            datasets: [{
+                backgroundColor: chartColors.red,
+                borderColor: chartColors.red,
+                data: [
+                    initial
+                ],
+                fill: false,
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            title: {
+                display: true,
+                text: name
+            },
+            legend: {
+                display: false,
+            },
+            tooltips: {
+                enabled: false,
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Value (USD)'
+                    }
+                }]
+            }
+        }
+    };
+
+    var ctx = document.getElementById('canvas').getContext('2d');
+    window[name] = new Chart(ctx, config);
+}
+
+getNextValue = (prev) => prev * (1.0 + (Math.random() - 0.5) * 0.01);
+
+function updateChart(name){
+    let d = new Date();
+    window[name].data.labels.push(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
+    
+    window[name].data.datasets.forEach(function(dataset) {
+        let next = getNextValue(dataset.data[dataset.data.length - 1]);
+        dataset.data.push(next);
+
+        window[name].update();
+
+        if(dataset.data.length >= 60){
+            window[name].data.labels.splice(0, 1); // remove the label first
+            dataset.data.shift();
+
+            window[name].update();
+        }
+    });
+}
