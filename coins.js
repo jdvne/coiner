53afa864a3d74c13a67b887948cd8bde1f075813
Joshua Devine <joshuaddevine@gmail.com>
2021-03-17 13:13:47 -0400

    Changes in last meeting

--- coins.js
+++ coins.js
@@ -1,23 +1,62 @@
-chartColors = {
-	red: 'rgb(128, 0, 0)',
-	orange: 'rgb(255, 159, 64)',
-	yellow: 'rgb(255, 205, 86)',
-	green: 'rgb(0, 128, 0)',
-	blue: 'rgb(54, 162, 235)',
-	purple: 'rgb(153, 102, 255)',
-	grey: 'rgb(201, 203, 207)'
+var initial_value = {
+    "My Portfolio": 2500,
+    "Bitcoin": 50000,
+    "Etherium": 1500,
+    "Dogecoin": 0.005,
+    "Litecoin": 20,
+    "Cardano": 0.15,
+    "Polkadot": 38,
+    "Bitcoin Cash": 550,
+    "Stellar": 0.42,
+    "Chainlink": 30
 };
 
-function makeChart(name, initial){
+var current_value = {
+    "My Portfolio": 2500,
+    "Bitcoin": 50000,
+    "Etherium": 1500,
+    "Dogecoin": 0.005,
+    "Litecoin": 20,
+    "Cardano": 0.15,
+    "Polkadot": 38,
+    "Bitcoin Cash": 550,
+    "Stellar": 0.42,
+    "Chainlink": 30
+}
+
+getNextValue = (prev) => prev * (1.0 + (Math.random() - 0.5) * 0.01);
+
+function updateCoinValues(coins){
+    coins.forEach(function(coin){
+        current_value[coin] = getNextValue(current_value[coin]);
+        console.log(current_value[coin]);
+    });
+}
+
+let fixDecimals = function(value){
+    let abs_val = Math.abs(value);
+    if(abs_val > 1000){
+        value = value.toFixed(0);
+    }else if(abs_val > 100){
+        value = value.toFixed(2);
+    }else if(abs_val > 1){
+        value = value.toFixed(3)
+    } else {
+        value = value.toFixed(6);
+    }
+    return value;
+}
+
+function makeChart(coin){
     var config = {
         type: 'line',
         data: {
             labels: [""],
             datasets: [{
-                backgroundColor: chartColors.red,
-                borderColor: chartColors.red,
+                backgroundColor: "green",
+                borderColor: "green",
                 data: [
-                    initial
+                    initial_value[coin]
                 ],
                 fill: false,
             }]
@@ -50,44 +89,26 @@ function makeChart(name, initial){
     };
 
     var ctx = document.getElementById('canvas').getContext('2d');
-    window[name] = new Chart(ctx, config);
-
-    window.setInterval(function(){ updateChart(name); }, 1000);
+    window[coin] = new Chart(ctx, config);
 }
 
-getNextValue = (prev) => prev * (1.0 + (Math.random() - 0.5) * 0.01);
-
-let fixDecimals = function(value){
-    if(value > 1000){
-        value = value.toFixed(0);
-    }else if(value > 100){
-        value = value.toFixed(2);
-    }else if(value > 1){
-        value = value.toFixed(3)
-    } else {
-        value = value.toFixed(6);
-    }
-    return value;
-}
-
-function updateChart(name){
+function updateChart(coin){
     let d = new Date();
-    window[name].data.labels.push(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
+    window[coin].data.labels.push(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
     
-    window[name].data.datasets.forEach(function(dataset) {
-        let next = getNextValue(dataset.data[dataset.data.length - 1]);
-        dataset.data.push(next);
-
-        window[name].update();
+    window[coin].data.datasets.forEach(function(dataset) {
+        var curr = current_value[coin];
+        dataset.data.push(curr);
+        window[coin].update();
 
         if(dataset.data.length >= 60){
-            window[name].data.labels.splice(0, 1); // remove the label first
+            window[coin].data.labels.splice(0, 1);
             dataset.data.shift();
 
-            window[name].update();
+            window[coin].update();
         }
 
-        if(dataset.data[0] > next){
+        if(dataset.data[0] > curr){
             dataset.borderColor = "red";
             dataset.backgroundColor = "red";
         }else{
@@ -95,53 +116,18 @@ function updateChart(name){
             dataset.backgroundColor = "green";
         }
 
-        window[name].update();
+        window[coin].update();
     });
 }
 
-function populateList(coins){
-    var list = document.getElementById("coin-list");
-    
-    for(const coin in coins){
-        list.insertAdjacentHTML('beforeend', 
-        "<tr id='" + coin + "'>" +
-            "<th scope='row'></th>" +
-            "<td>" + coin + "</td>" +
-            "<td id='value'>" + coins[coin] + "</td>" +
-            "<td id='change'></td>" +
-        "</tr>");
-    }
-
-    window.setInterval(function(){ updateList(coins); }, 1000);
-}
-
-function updateList(coins){
-    for (const coin in coins){
-        var row = document.getElementById(coin);
-
-        var curr = parseFloat(row.childNodes.item(2).textContent);
-        var next = fixDecimals(getNextValue(curr));
-        var diff = fixDecimals(next - coins[coin]);
-        
-        row.childNodes.item(2).textContent = next;
-
-        if (diff > 0){
-            row.childNodes.item(3).textContent = "▲ " + diff;
-            row.style = "color: green";
-        } else {
-            row.childNodes.item(3).textContent = "▼ " + Math.abs(diff);
-            row.style = "color: red";
-        }
-    }
-}
-
-function updateFeaturedCoin(coins, name){
+function updateFeaturedCoin(coin){
     var featured_coin = document.getElementById("featured-dc");
 
-    window[name].data.datasets.forEach(function(dataset) {
-        var curr = dataset.data[dataset.data.length - 1];
-        var diff = fixDecimals(curr - coins[name]);
-        var pct = (Math.abs(diff) / coins[name]).toFixed(1);
+    window[coin].data.datasets.forEach(function(dataset) {
+        var init = initial_value[coin];
+        var curr = current_value[coin];
+        var diff = fixDecimals(curr - init);
+        var pct = ((Math.abs(diff) / init) * 100).toFixed(1);
 
         if (diff > 0){
             featured_coin.textContent = "▲ " + diff + " (" + pct + "%)";
@@ -149,6 +135,40 @@ function updateFeaturedCoin(coins, name){
         } else {
             featured_coin.textContent = "▼ " + Math.abs(diff) + " (" + pct + "%)";
             featured_coin.style = "color: red";
+        }
+    });
+}
+
+function makeList(coins){
+    var list = document.getElementById("coin-list");
+    
+    coins.forEach(function(coin){
+        list.insertAdjacentHTML('beforeend', 
+        "<tr id='" + coin + "' class='focus-row'>" +
+            "<th scope='row'></th>" +
+            "<td>" + coin + "</td>" +
+            "<td id='value'>" + initial_value[coin] + "</td>" +
+            "<td id='change'></td>" +
+        "</tr>");
+    });
+}
+
+function updateList(coins){
+    coins.forEach(function(coin){
+        var row = document.getElementById(coin);
+
+        var init = initial_value[coin];
+        var curr = current_value[coin];
+        var diff = fixDecimals(curr - init);
+        
+        row.childNodes.item(2).textContent = fixDecimals(curr);
+
+        if (diff > 0){
+            row.childNodes.item(3).textContent = "▲ " + diff;
+            row.style = "color: green";
+        } else {
+            row.childNodes.item(3).textContent = "▼ " + Math.abs(diff);
+            row.style = "color: red";
         }
     });
 }
--- search.js
+++ search.js
@@ -1,7 +1,18 @@
-function search_crypto() { 
+function makeSearchList(coins){
+    var list = document.getElementById("coin-list");
+    
+    coins.forEach(function(coin){
+        list.insertAdjacentHTML('beforeend',
+        "<li class='crypto'>" + coin + "<br/><br/></li>");
+    });
+}
+
+/* Code based on https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/*/ 
+function searchCoins() {
     search_entry = document.getElementById('the-searchbar').value;
-    crypto_name = document.getElementsByClassName('crypto');  
-    search_entry = search_entry.toLowerCase();   
+    crypto_name = document.getElementsByClassName('crypto');
+    search_entry = search_entry.toLowerCase();
+
     for (i = 0; i < crypto_name.length; i++) {  
         if (crypto_name[i].innerHTML.toLowerCase().includes(search_entry)) { 
             crypto_name[i].style.display="list-item"; 
@@ -12,4 +23,4 @@ function search_crypto() {
             
         }  
     } 
-} 
+}
